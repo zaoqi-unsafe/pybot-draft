ca22ec80b5a13a3551289eadc05de390a56706ac
zaoqi-unsafe <zaoqi-unsafe@users.noreply.github.com>
2019-01-30 20:24:55 +0800

    Add files via upload

--- bot.py
+++ bot.py
@@ -11,8 +11,18 @@ def wxbot_receive_raw(rawmsg):
 	msg['text'] = rawmsg.text
 	receive_wx_text(msg)
 
+from aiocqhttp import CQHttp
+
+qqbot = CQHttp(access_token='',
+             enable_http_post=False)
+
+@qqbot.on_message()
+async def qq_handle_msg(context):
+    await qqbot.send(context, '你好呀，下面一条是你刚刚发的：')
+    return {'reply': context['message']}
+
 def receive_wx_text(msg):
 	wxgroup.send('received:[{}] {}'.format(msg['sender'], msg['text']))
 
-
+qqbot.run(host='127.0.0.1', port=8194)
 wxbot.join()
